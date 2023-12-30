e90cf1323a65427057120b6a9c1a75450d391716
tobi <mail.tobias.leibrock@gmail.com>
2023-12-30 01:19:05 +0100

    work on dataset, model and finalize data transformations

--- model/model.py
+++ model/model.py
@@ -1,21 +1,44 @@
-from torchvision.io import read_image
 from torchvision.models import resnet18, ResNet18_Weights
+import torchvision
+import torch
+import torch.utils.data
+import torch.nn as nn
+import torch.optim as optim
+import torch.optim.lr_scheduler as lr_scheduler
+from dataset import BlockingObservationalDataset
+from train import train_model
 
-img = read_image("test/assets/encode_jpeg/grace_hopper_517x606.jpg")
 
-model = resnet18(pretrained=True)
-model.eval()
+model = resnet18(weights=ResNet18_Weights.DEFAULT)
 
-# Step 2: Initialize the inference transforms
-preprocess = weights.transforms()
+# create new first conv layer
+conv1_weights = model.conv1.weight.clone()
+model.conv1 = nn.Conv2d(5, 64, kernel_size=7, stride=2, padding=3, bias=False)
+with torch.no_grad():
+    model.conv1.weight[:, :3] = conv1_weights
+    model.conv1.weight[:, 3] = model.conv1.weight[:, 0]
+    model.conv1.weight[:, 4] = model.conv1.weight[:, 0]
 
-# Step 3: Apply inference preprocessing transforms
-batch = preprocess(img).unsqueeze(0)
+# disable gradient descent for all pre-trained parameters
+for parameters in model.parameters():
+    parameters.requires_grad = False
 
-# Step 4: Use the model and print the predicted category
-prediction = model(batch).squeeze(0).softmax(0)
-class_id = prediction.argmax().item()
-score = prediction[class_id].item()
-category_name = weights.meta["categories"][class_id]
-print(f"{category_name}: {100 * score:.1f}%")
+# create new final linear layer
+fully_features = model.fc.in_features
+model.fc = nn.Sequential(nn.Linear(fully_features, 1), nn.ReLU())
 
+model.double()
+
+criterion = nn.BCELoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001)
+
+scheduler = lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.1)
+
+training_dataset = BlockingObservationalDataset()
+training_loader = torch.utils.data.DataLoader(
+    training_dataset, batch_size=4, shuffle=True
+)
+
+dataloaders = {"train": training_loader, "val": training_loader}
+
+train_model(model, criterion, optimizer, scheduler, dataloaders, 1)
